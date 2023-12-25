7b76b40f8c6a7d522adc2807c00ef300da7b65f0
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2023-12-26 04:55:31 +0800

    :sparkles: 实现 `Token` 到 `Value` 的转换，完成解析功能
    为 `Value` 实现了 `TryFrom<Token>`，并加入了特殊数据的转换
    比如布尔值 `#t` 和各进制数字 `#b` `#o` `#d` `#x`

--- src/model/value.rs
+++ src/model/value.rs
@@ -1,4 +1,6 @@
-use rug::{Float, Integer};
+use rug::{Complete, Float, Integer};
+
+use super::{ParseError, Token};
 
 pub enum Value {
     Void,
@@ -6,6 +8,44 @@ pub enum Value {
     Float(Float),
     Bool(bool),
     Symbol(String),
+    String(String),
     List(Vec<Value>),
     Quoted(Box<Value>),
+}
+
+impl TryFrom<Token> for Value {
+    type Error = ParseError;
+
+    fn try_from(token: Token) -> Result<Self, Self::Error> {
+        match token {
+            Token::LParen | Token::RParen | Token::Quote => {
+                Err(ParseError::NonConvertibleToken(token))
+            }
+
+            Token::Integer(i) => Ok(Value::Integer(i)),
+            Token::Float(f) => Ok(Value::Float(f)),
+            Token::String(s) => Ok(Value::String(s)),
+
+            Token::Symbol(symbol) => match symbol.as_str() {
+                "#t" => Ok(Value::Bool(true)),
+                "#f" => Ok(Value::Bool(false)),
+                s if s.starts_with('#') => {
+                    let radix = match s.chars().nth(1) {
+                        Some('b') => 2,
+                        Some('o') => 8,
+                        Some('d') => 10,
+                        Some('x') => 16,
+                        _ => return Err(ParseError::InvalidSyntax(Token::Symbol(symbol))),
+                    };
+                    let digits = &s[2..];
+                    let value = Integer::parse_radix(digits, radix)
+                        .map_err(|_| ParseError::InvalidDigit(digits.into()))?
+                        .complete();
+
+                    Ok(Value::Integer(value))
+                }
+                _ => Ok(Value::Symbol(symbol)),
+            },
+        }
+    }
 }
