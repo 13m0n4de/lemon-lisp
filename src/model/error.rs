83ba2aa2d5ca3193b47a8baf75ffdb45143c7536
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2023-12-26 23:49:19 +0800

    :sparkles: [Error] 为 `TokenizeError` 和 `ParseError` 实现 `fmt::Display` 特征

--- src/model/error.rs
+++ src/model/error.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 use super::Token;
 
 /// 词法分析中可能发生的错误
@@ -22,5 +24,50 @@ pub enum ParseError {
 impl From<TokenizeError> for ParseError {
     fn from(value: TokenizeError) -> Self {
         ParseError::LexicalError(value)
+    }
+}
+
+impl fmt::Display for TokenizeError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TokenizeError::UnexpectedChar(c) => {
+                write!(f, "Unexpected character: {}", c)
+            }
+            TokenizeError::UnclosedString => {
+                write!(f, "Unclosed string")
+            }
+        }
+    }
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ParseError::UnexpectedToken { expected, found } => {
+                write!(
+                    f,
+                    "Unexpected token: expected {}, found {}",
+                    expected, found
+                )
+            }
+            ParseError::MissingToken(token_type) => {
+                write!(f, "Missing token: {}", token_type)
+            }
+            ParseError::InvalidSyntax(token) => {
+                write!(f, "Invalid syntax: {}", token)
+            }
+            ParseError::InvalidDigit(digit) => {
+                write!(f, "Invalid digit: {}", digit)
+            }
+            ParseError::LexicalError(error) => {
+                write!(f, "Lexical error: {}", error)
+            }
+            ParseError::NonConvertibleToken(token) => {
+                write!(f, "Non-convertible token: {}", token)
+            }
+            ParseError::UnexpectedEOF => {
+                write!(f, "Unexpected EOF")
+            }
+        }
     }
 }
