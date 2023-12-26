4ce17479a5e9d797f3bce2c126431ccfcf52942b
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2023-12-26 23:23:37 +0800

    :memo: [Value] 添加文档

--- src/model/value.rs
+++ src/model/value.rs
@@ -2,6 +2,8 @@ use rug::{Complete, Float, Integer};
 
 use super::{ParseError, Token};
 
+/// [`Value`] 枚举包含了所有可能的 Lisp 值，包括原子、列表等等。
+#[derive(Debug, PartialEq)]
 pub enum Value {
     Void,
     Integer(Integer),
@@ -16,6 +18,50 @@ pub enum Value {
 impl TryFrom<Token> for Value {
     type Error = ParseError;
 
+    /// 尝试将 [`Token`] 转换为 [`Value`]。
+    ///
+    /// # 示例
+    /// ## 将整数 Token 转换为 Value
+    ///
+    /// ```rust
+    /// # use rug;
+    /// # use lemon_lisp::model::{Token, Value};
+    /// #
+    /// let integer_token = Token::Integer(rug::Integer::from(123));
+    /// 
+    /// assert_eq!(
+    ///     Ok(Value::Integer(rug::Integer::from(123))),
+    ///     Value::try_from(integer_token),
+    /// );
+    /// ```
+    ///
+    /// ## 将符号 Token 转换为 Value
+    ///
+    /// ```rust
+    /// # use lemon_lisp::model::{Token, Value};
+    /// #
+    /// let symbol_token = Token::Symbol("#t".to_string());
+    ///
+    /// assert_eq!(
+    ///     Ok(Value::Bool(true)),
+    ///     Value::try_from(symbol_token)
+    /// );
+    /// ```
+    ///
+    /// # 错误
+    ///
+    /// 当遇到错误语法或不可转换的 [`Token`] 时，会返回 [`ParseError`] 。
+    ///
+    /// ```rust
+    /// # use lemon_lisp::model::{Token, Value, ParseError};
+    /// #
+    /// let invalid_token = Token::RParen;
+    ///
+    /// assert_eq!(
+    ///     Err(ParseError::NonConvertibleToken(Token::RParen)),
+    ///     Value::try_from(invalid_token)
+    /// );
+    /// ```
     fn try_from(token: Token) -> Result<Self, Self::Error> {
         match token {
             Token::LParen | Token::RParen | Token::Quote => {
