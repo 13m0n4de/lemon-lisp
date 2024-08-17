656aa4b9b9c10ce5a30bdad9342be096c60d3a17
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-08-17 20:41:00 +0800

    test(evaluator): refactor tests

--- tests/evaluator_test.rs
+++ tests/evaluator_test.rs
@@ -12,38 +12,41 @@ mod tests {
     };
     use rug::Integer;
 
+    fn eval_input(
+        input: &str,
+        evaluator: &Evaluator,
+        environment: &Rc<Environment>,
+    ) -> Result<Value, RuntimeError> {
+        let token_stream = TokenStream::new(input);
+        let mut parser = Parser::new(token_stream);
+        let parse_result = parser.parse()?;
+
+        parse_result
+            .into_iter()
+            .map(|value| evaluator.eval_value(&value, environment))
+            .last()
+            .unwrap_or(Ok(Value::Void))
+    }
+
     #[test]
     fn test_define_var() {
-        let token_stream = TokenStream::new("(define a 2)");
-        let mut parser = Parser::new(token_stream);
-        let parse_result = parser.parse();
-
-        assert!(parse_result.is_ok());
-
         let environment = Environment::new();
         let evaluator = Evaluator;
-        for value in parse_result.unwrap() {
-            let evaluate_result = evaluator.eval_value(&value, &environment);
-            assert_eq!(Ok(Value::Void), evaluate_result);
-        }
 
+        let input = "(define a 2)";
+        let result = eval_input(input, &evaluator, &environment);
+
+        assert!(result.is_ok());
         assert_eq!(Some(Value::from(Integer::from(2))), environment.get("a"));
     }
 
     #[test]
     fn test_define_closure() {
-        let token_stream = TokenStream::new("(define (add-one n) (+ n 1))");
-        let mut parser = Parser::new(token_stream);
-        let parse_result = parser.parse();
-
-        assert!(parse_result.is_ok());
-
         let environment = Environment::new();
         let evaluator = Evaluator;
-        for value in parse_result.unwrap() {
-            let evaluate_result = evaluator.eval_value(&value, &environment);
-            assert_eq!(Ok(Value::Void), evaluate_result);
-        }
+
+        let result = eval_input("(define (add-one n) (+ n 1))", &evaluator, &environment);
+        assert!(result.is_ok());
 
         if let Some(Value::Closure(closure)) = environment.get("add-one") {
             assert_eq!(Some("add-one".to_string()), closure.name);
@@ -64,83 +67,63 @@ mod tests {
 
     #[test]
     fn test_lambda() {
-        let token_stream = TokenStream::new("(lambda (a b) (+ a b))");
-        let mut parser = Parser::new(token_stream);
-        let parse_result = parser.parse();
-
-        assert!(parse_result.is_ok());
-
         let environment = Environment::new();
         let evaluator = Evaluator;
-        let value = &parse_result.unwrap()[0];
-        let evaluate_result = evaluator.eval_value(value, &environment);
 
-        match evaluate_result {
-            Ok(Value::Closure(closure)) => {
-                assert_eq!(None, closure.name);
-                assert_eq!(vec!["a".to_string(), "b".to_string()], closure.params);
-                assert_eq!(
-                    vec![Value::List(vec![
-                        Value::Symbol("+".into()),
-                        Value::Symbol("a".into()),
-                        Value::Symbol("b".into()),
-                    ])],
-                    closure.body
-                );
-                assert!(closure.environment.upgrade().is_some());
-            }
-            _ => panic!("Expected a closure"),
+        let result = eval_input("(lambda (a b) (+ a b))", &evaluator, &environment);
+
+        assert!(result.is_ok());
+        if let Ok(Value::Closure(closure)) = result {
+            assert_eq!(None, closure.name);
+            assert_eq!(vec!["a".to_string(), "b".to_string()], closure.params);
+            assert_eq!(
+                vec![Value::List(vec![
+                    Value::Symbol("+".into()),
+                    Value::Symbol("a".into()),
+                    Value::Symbol("b".into()),
+                ])],
+                closure.body
+            );
+            assert!(closure.environment.upgrade().is_some());
+        } else {
+            panic!("Expected a closure");
         }
     }
 
     #[test]
     fn test_optimize_tail_call() {
-        let token_stream = TokenStream::new("(define (loop) (loop))");
-        let mut parser = Parser::new(token_stream);
-        let parse_result = parser.parse();
-
-        assert!(parse_result.is_ok());
-
         let environment = Environment::new();
         let evaluator = Evaluator;
-        let value = &parse_result.unwrap()[0];
-        let evaluate_result = evaluator.eval_value(value, &environment);
 
-        assert_eq!(Ok(Value::Void), evaluate_result);
-        match environment.get("loop") {
-            Some(Value::TailCall(tail_call)) => {
-                assert_eq!(Some("loop".to_string()), tail_call.closure.name);
-                assert!(tail_call.closure.params.is_empty());
-                assert!(tail_call.closure.body.is_empty());
-                assert!(tail_call.closure.environment.upgrade().is_some());
-                assert!(tail_call.updates.is_empty());
-                assert_eq!(Value::Bool(false), *tail_call.break_condition);
-                assert_eq!(Value::Void, *tail_call.return_expr);
-            }
-            _ => panic!("Expected a tail call"),
-        };
+        let result = eval_input("(define (loop) (loop))", &evaluator, &environment);
+
+        assert!(result.is_ok());
+        if let Some(Value::TailCall(tail_call)) = environment.get("loop") {
+            assert_eq!(Some("loop".to_string()), tail_call.closure.name);
+            assert!(tail_call.closure.params.is_empty());
+            assert!(tail_call.closure.body.is_empty());
+            assert!(tail_call.closure.environment.upgrade().is_some());
+            assert!(tail_call.updates.is_empty());
+            assert_eq!(Value::Bool(false), *tail_call.break_condition);
+            assert_eq!(Value::Void, *tail_call.return_expr);
+        } else {
+            panic!("Expected a tail call");
+        }
     }
 
     #[test]
     fn test_internal_fn() {
-        let token_stream = TokenStream::new("(+ 2 3)");
-        let mut parser = Parser::new(token_stream);
-        let parse_result = parser.parse();
+        let environment = Environment::new();
+        let evaluator = Evaluator;
 
-        assert!(parse_result.is_ok());
-
-        // (+ num1 num2 num3) => 0 + num1 + num2 + num3
         let add = |args: &[Value], _: &Rc<Environment>| -> Result<Value, RuntimeError> {
-            let result = args
-                .iter()
+            args.iter()
                 .try_fold(Numeric::Integer(0.into()), |acc, arg| {
                     arg.try_as_numeric().map(|n| n + acc)
-                })?;
-
-            Ok(result.into())
+                })
+                .map(Into::into)
         };
 
-        let environment = Environment::new();
         environment.set(
             "+",
             Value::InternalFunction(InternalFunction {
@@ -149,10 +132,8 @@ mod tests {
             }),
         );
 
-        let evaluator = Evaluator;
-        let value = &parse_result.unwrap()[0];
-        let evaluate_result = evaluator.eval_value(value, &environment);
+        let result = eval_input("(+ 2 3)", &evaluator, &environment);
 
-        assert_eq!(Ok(Value::from(Integer::from(5))), evaluate_result);
+        assert_eq!(Ok(Value::from(Integer::from(5))), result);
     }
 }
